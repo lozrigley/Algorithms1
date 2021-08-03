73875dcc118eeef8c17acc1866985f8152d5090a
lozrigley <laurence.rigley@comparisontech.com>
2021-08-03 17:58:29 +0100

    fixed shoddy code

--- src/App.java
+++ src/App.java
@@ -1,8 +1,6 @@
-import edu.princeton.cs.algs4.StdIn;
-
 public class App {
     public static void main(String[] args) throws Exception {
-        var text = edu.princeton.cs.algs4.StdIn.readString();
+        String text = edu.princeton.cs.algs4.StdIn.readString();
         System.out.println("Hello " + text);
     }
 }
