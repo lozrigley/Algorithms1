40e098467792dcff98c93822dc6fa649123e96a7
lozrigley <laurence.rigley@comparisontech.com>
2021-08-03 18:05:20 +0100

    where did that come from??

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         String text = edu.princeton.cs.algs4.StdIn.readString();
         System.out.println("Hello " + text);
     }
