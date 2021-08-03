3515ca72730e445fe9abf8664b2bc87e2eb92dcb
lozrigley <laurence.rigley@comparisontech.com>
2021-08-03 18:07:17 +0100

    code to just cause error

--- src/App.java
+++ src/App.java
@@ -1,6 +1,8 @@
+
+import edu.princeton.cs.algs4.StdLn;
 public class App {
     public static void main(String[] args) {
-        String text = edu.princeton.cs.algs4.StdIn.readString();
-        System.out.println("Hello " + text);
+        //String text = edu.princeton.cs.algs4.StdIn.readString();
+        System.out.println("Hello ");
     }
 }
